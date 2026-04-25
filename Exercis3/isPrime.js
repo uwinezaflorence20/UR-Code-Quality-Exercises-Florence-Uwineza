e8a02da1808141fc280b5f5443065eab8853adfe
uwinezaflorence20 <uwinezaflorence20@gmail.com>
2026-04-25 23:21:25 +0200

    change n to number for better understanding

--- Exercis3/isPrime.js
+++ Exercis3/isPrime.js
@@ -1,11 +1,11 @@
 // Write a JavaScript function to check if a given number is prime or not.
-function isPrime(n) {
-  if (n <= 1) return false;     
-  if (n === 2) return true;      
-  if (n % 2 === 0) return false; 
+function isPrime(number) {
+  if (number <= 1) return false;     
+  if (number === 2) return true;      
+  if (number % 2 === 0) return false; 
 
-  for (let i = 3; i <= Math.sqrt(n); i += 2) {
-    if (n % i === 0) {
+  for (let i = 3; i <= Math.sqrt(number); i += 2) {
+    if (number % i === 0) {
       return false;
     }
   }
